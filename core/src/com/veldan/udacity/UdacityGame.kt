d37a464c4b21cc48b34b3cccfd5e87beebf7d1d9
Vlad Shesternin <valdon1202@gmail.com>
2021-09-18 14:21:02 +0300

    ShapeRenderer 1

--- core/src/com/veldan/udacity/UdacityGame.kt
+++ core/src/com/veldan/udacity/UdacityGame.kt
@@ -1,27 +1,31 @@
 package com.veldan.udacity
 
 import com.badlogic.gdx.ApplicationAdapter
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.utils.ScreenUtils
+import kotlin.random.Random
 
 class UdacityGame : ApplicationAdapter() {
-    var batch: SpriteBatch? = null
-    var img: Texture? = null
-    override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
-    }
+    private val shapeRenderer by lazy { ShapeRenderer() }
+
+    private val width by lazy { Gdx.graphics.width }
+    private val height by lazy { Gdx.graphics.height }
+
+    val x by lazy { List<Float>(300) { (1..width).random().toFloat() } }
+    val y by lazy { List<Float>(300) { (1..height).random().toFloat() } }
 
     override fun render() {
-        ScreenUtils.clear(1f, 0f, 0f, 1f)
-        batch!!.begin()
-        batch!!.draw(img, 0f, 0f)
-        batch!!.end()
+        ScreenUtils.clear(Color.BLACK)
+        with(shapeRenderer) {
+            begin(ShapeRenderer.ShapeType.Point)
+            x.zip(y).onEach { point(it.first, it.second, 0f) }
+            end()
+        }
     }
 
     override fun dispose() {
-        batch!!.dispose()
-        img!!.dispose()
+        shapeRenderer.dispose()
     }
 }
---
+++ core/src/com/veldan/udacity/Util.kt
@@ -0,0 +1,5 @@
+package com.veldan.udacity
+
+import com.badlogic.gdx.Gdx
+
+fun log(message: String) = Gdx.app.log("TAG", message)
