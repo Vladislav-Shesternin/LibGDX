134610c7bd23019a68bb0241ccd56f0314d00b7f
Vlad Shesternin <valdon1202@gmail.com>
2021-09-18 11:04:47 +0300

    Start Udacity Course

---
+++ desktop/src/com/veldan/udacity/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.veldan.udacity.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.veldan.udacity.UdacityGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(UdacityGame(), config)
+    }
+}
