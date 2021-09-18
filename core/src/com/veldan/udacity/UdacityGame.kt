5edad2c0e16719e465459b7d549406e2598d3775
Vlad Shesternin <valdon1202@gmail.com>
2021-09-18 14:38:19 +0300

    ShapeRenderer lines

--- core/src/com/veldan/udacity/UdacityGame.kt
+++ core/src/com/veldan/udacity/UdacityGame.kt
@@ -13,14 +13,21 @@ class UdacityGame : ApplicationAdapter() {
     private val width by lazy { Gdx.graphics.width }
     private val height by lazy { Gdx.graphics.height }
 
-    val x by lazy { List<Float>(300) { (1..width).random().toFloat() } }
-    val y by lazy { List<Float>(300) { (1..height).random().toFloat() } }
+    val xy = floatArrayOf(
+            300f, 100f,
+            200f, 200f,
+            400f, 400f,
+            500f, 300f,
+    )
 
     override fun render() {
         ScreenUtils.clear(Color.BLACK)
         with(shapeRenderer) {
-            begin(ShapeRenderer.ShapeType.Point)
-            x.zip(y).onEach { point(it.first, it.second, 0f) }
+            begin(ShapeRenderer.ShapeType.Line)
+            line(10f, 10f, 100f, 100f, Color.RED, Color.BLUE)
+            color = Color.BLUE
+            line(20f, 10f, 110f, 100f)
+            polyline(xy)
             end()
         }
     }
