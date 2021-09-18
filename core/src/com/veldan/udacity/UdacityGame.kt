77d5aba3d8cfeba740ce2d132486224123534ac0
Vlad Shesternin <valdon1202@gmail.com>
2021-09-18 14:59:03 +0300

    ShapeRenderer human polyline

--- core/src/com/veldan/udacity/UdacityGame.kt
+++ core/src/com/veldan/udacity/UdacityGame.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.utils.ScreenUtils
 import kotlin.random.Random
 
@@ -13,21 +14,45 @@ class UdacityGame : ApplicationAdapter() {
     private val width by lazy { Gdx.graphics.width }
     private val height by lazy { Gdx.graphics.height }
 
-    val xy = floatArrayOf(
-            300f, 100f,
-            200f, 200f,
-            400f, 400f,
-            500f, 300f,
+    val human = floatArrayOf(
+            100f, 10f,
+            100f, 50f,
+
+            90f, 50f,
+            90f, 100f,
+
+            100f, 100f,
+            100f, 110f,
+
+            80f, 110f,
+            80f, 130f,
+
+            150f, 130f,
+            150f, 110f,
+
+            130f, 110f,
+            130f, 100f,
+
+            140f, 100f,
+            140f, 50f,
+
+            130f, 50f,
+            130f, 10f,
+
+            117f, 10f,
+            117f, 40f,
+
+            113f, 40f,
+            113f, 10f,
+
+            100f, 10f,
     )
 
     override fun render() {
         ScreenUtils.clear(Color.BLACK)
         with(shapeRenderer) {
             begin(ShapeRenderer.ShapeType.Line)
-            line(10f, 10f, 100f, 100f, Color.RED, Color.BLUE)
-            color = Color.BLUE
-            line(20f, 10f, 110f, 100f)
-            polyline(xy)
+            polyline(human)
             end()
         }
     }
