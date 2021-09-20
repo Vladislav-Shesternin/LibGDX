44f9b55216f7ec6b36263e09e22e141bb9970aea
Vlad Shesternin <valdon1202@gmail.com>
2021-09-20 12:17:17 +0300

    Font

--- core/src/com/veldan/udacity/UdacityGame.kt
+++ core/src/com/veldan/udacity/UdacityGame.kt
@@ -1,50 +1,69 @@
 package com.veldan.udacity
 
 import com.badlogic.gdx.ApplicationAdapter
-import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.utils.Array
 import com.badlogic.gdx.utils.ScreenUtils
 import com.badlogic.gdx.utils.viewport.*
 
 class UdacityGame : ApplicationAdapter() {
 
     companion object {
-        const val WIDTH = 100f
-        const val HEIGHT = 50f
+        private const val WIDTH = 100f
+        private const val HEIGHT = 50f
     }
 
-    private val shapeRenderer by lazy { ShapeRenderer() }
-    private val viewport by lazy { FitViewport(WIDTH, HEIGHT) }
+    private val batch by lazy { SpriteBatch() }
+    private val font by lazy { BitmapFont() }
 
-    override fun resize(width: Int, height: Int) {
-        viewport.update(width, height, true)
-    }
+    val words = generateWords(20)
 
     override fun render() {
-        ScreenUtils.clear(Color.BLUE)
-        viewport.camera.update()
-
-        shapeRenderer.apply {
-            projectionMatrix = viewport.camera.combined
-            begin(ShapeType.Filled)
-
-            color = Color.YELLOW
-            circle(WIDTH / 2f, HEIGHT / 2f, HEIGHT / 2, 1000)
-            color = Color.BLACK
-            circle(WIDTH * 0.4f, HEIGHT * 0.7f, HEIGHT * 0.1f, 1000)
-            circle(WIDTH * 0.6f, HEIGHT * 0.7f, HEIGHT * 0.1f, 1000)
-            arc(WIDTH / 2f, HEIGHT / 2f, HEIGHT / 4f, -180f, 180f, 1000)
-            color = Color.YELLOW
-            arc(WIDTH / 2f, HEIGHT / 2f, HEIGHT / 6f, -180f, 180f, 1000)
-            end()
+        ScreenUtils.clear(Color.TEAL)
+        with(batch) {
+            begend {
+                words.onEach {
+                    font.data.scale(it.scale)
+                    font.color = it.color
+                    font.draw(batch, it.letters, it.x, it.y)
+                }
+            }
         }
     }
 
     override fun dispose() {
-        shapeRenderer.dispose()
+        batch.dispose()
+        font.dispose()
     }
+
+}
+
+private fun generateWords(count: Int): List<Word> = MutableList<Word>(count) {
+    Word.randomWord(0.5f, 3f)
+}
+
+data class Word(
+        var x: Float,
+        var y: Float,
+        var scale: Float,
+        var color: Color,
+        var letters: String
+) {
+
+    companion object {
+        private val WORDS = listOf("render-farm", "refrigerator", "tiger-team", "weathered", "camera", "tattoo", "boat", "soul-delay", "nodal point", "motion augmented", "reality neon", "nano-construct", "garage", "bicycle", "rebar tanto", "modem", "concrete RAF", "industrial grade media", "realism", "drone", "post-franchise shoes", "render-farm-ware", "DIY San Francisco", "rain lights", "numinous tank-traps", "pen drone", "cyber-cardboard", "denim monofilament", "order-flow", "smart-hotdog")
+
+        fun randomWord(minScale: Float, maxScale: Float): Word {
+            val x = MathUtils.random(-.25f, .75f)
+            val y = MathUtils.random()
+            val scale = minScale + (maxScale - minScale) * MathUtils.random()
+            val color = Color(MathUtils.random(), MathUtils.random(), MathUtils.random(), 1f)
+            val letters = WORDS[MathUtils.random(WORDS.size - 1)]
+            return Word(x, y, scale, color, letters)
+        }
+    }
+
 }
--- core/src/com/veldan/udacity/Util.kt
+++ core/src/com/veldan/udacity/Util.kt
@@ -1,5 +1,12 @@
 package com.veldan.udacity
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 fun log(message: String) = Gdx.app.log("TAG", message)
+
+fun SpriteBatch.begend(block: SpriteBatch.() -> Unit) {
+    begin()
+    block()
+    end()
+}
