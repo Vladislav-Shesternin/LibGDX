89ce31e88581e5b7b75cbff3e344382e32610aac
Vlad Shesternin <valdon1202@gmail.com>
2021-09-20 19:59:36 +0300

    Accelerometer

--- core/src/com/veldan/udacity/GameScreen.kt
+++ core/src/com/veldan/udacity/GameScreen.kt
@@ -1,40 +1,48 @@
 package com.veldan.udacity
 
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.Input
-import com.badlogic.gdx.ScreenAdapter
+import com.badlogic.gdx.*
+import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.Vector2
 import com.veldan.udacity.UdacityGame.Companion.HEIGHT
 import com.veldan.udacity.UdacityGame.Companion.WIDTH
 import com.veldan.udacity.UdacityGame.Companion.aspectRatio
 
-class GameScreen : ScreenAdapter() {
+class GameScreen : ScreenAdapter(), VeldanInputProcessor {
 
     private val shapeRenderer by lazy { ShapeRenderer() }
+    private val font by lazy { BitmapFont() }
+    private val batch by lazy { SpriteBatch() }
 
     var x = WIDTH * .10f
     var y = HEIGHT * .10f
 
-    override fun render(delta: Float) {
-        handleEvent()
+    override fun show() {
+        Gdx.input.inputProcessor = this
+    }
 
+    override fun render(delta: Float) {
         shapeRenderer.projectionMatrix = game.viewport.camera.combined
+        batch.projectionMatrix = game.viewport.camera.combined
+
+        batch.begend {
+            font.draw(batch, "x = ${Gdx.input.accelerometerX}; y = ${Gdx.input.accelerometerY}; z = ${Gdx.input.accelerometerZ};",0f, HEIGHT * .9f)
+        }
         with(shapeRenderer) {
             begin(ShapeRenderer.ShapeType.Filled)
-            color = Color.YELLOW
-            rect(0f, 0f, WIDTH * aspectRatio, HEIGHT)
-            color = Color.BLUE
-            circle(x, y, 10f)
+
             end()
         }
     }
 
-    private fun handleEvent() {
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) y += 1f
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) y -= 1f
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) x += 1f
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) x -= 1f
+    override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
+        val v = game.viewport.unproject(Vector2(screenX.toFloat(), screenY.toFloat()))
+        x = v.x
+        y = v.y
+        return false
     }
 
 }
--- core/src/com/veldan/udacity/Util.kt
+++ core/src/com/veldan/udacity/Util.kt
@@ -1,6 +1,7 @@
 package com.veldan.udacity
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 fun log(message: String) = Gdx.app.log("TAG", message)
@@ -9,4 +10,38 @@ fun SpriteBatch.begend(block: SpriteBatch.() -> Unit) {
     begin()
     block()
     end()
+}
+
+interface VeldanInputProcessor : InputProcessor {
+    override fun keyDown(keycode: Int): Boolean {
+        return false
+    }
+
+    override fun keyUp(keycode: Int): Boolean {
+        return false
+    }
+
+    override fun keyTyped(character: Char): Boolean {
+        return false
+    }
+
+    override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        return false
+    }
+
+    override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        return false
+    }
+
+    override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
+        return false
+    }
+
+    override fun mouseMoved(screenX: Int, screenY: Int): Boolean {
+        return false
+    }
+
+    override fun scrolled(amountX: Float, amountY: Float): Boolean {
+        return false
+    }
 }
